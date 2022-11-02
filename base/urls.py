55f03699d54853448b7cfb6c5181016528666b0b
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-11-02 18:21:45 +0100

    added views, updated urls

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+
+from base.models import Task
+from .views import TaskDetail, TaskList, TaskCreate
 
 urlpatterns = [
-    path('', views.tasks, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
